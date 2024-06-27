cab8f2ec42b79615c278f8b9c115196ad8db84b9
Arqam Shaikh <68507521+arqamcodes@users.noreply.github.com>
2024-06-27 18:17:36 +0500

    Update chatbotLogic.py

--- chatbotLogic.py
+++ chatbotLogic.py
@@ -1,15 +1,1 @@
-import google.generativeai as genai
 
-class Chatbot:
-    def __init__(self, api_key):
-        genai.configure(api_key=api_key)
-        self.model = genai.GenerativeModel('gemini-pro')
-        self.chat = self.model.start_chat(history=[])
-
-    def get_response(self, message):
-        try:
-            instructions = "In this chat, respond as you are Admission Information related chatbot of a university in Karachi,Pakistan called Dawood University of Engineering and Technology, give the answer  briefly and precisely"
-            response = self.chat.send_message(instructions + message)
-            return response.text
-        except Exception as e:
-            return f"An error occurred: {e}"
